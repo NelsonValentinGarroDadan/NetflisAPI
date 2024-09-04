17e261a466c0151add52ef5987d57e79faea87c3
Velentin Garro <nelsonvgarrodadan@gmail.com>
2024-09-04 17:28:07 -0300

    Update dbCon.js

--- src/config/dbCon.js
+++ src/config/dbCon.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
-const urlDB = "mongodb+srv://nelsonvgarrodadan:zg5efej0vPIXA0xV@workplase.0pzcr.mongodb.net/?retryWrites=true&w=majority&appName=WorkPlase/Movies"
+const urlDB = "mongodb+srv://nelsonvgarrodadan:zg5efej0vPIXA0xV@workplase.0pzcr.mongodb.net/Movies?retryWrites=true&w=majority&appName=WorkPlase"
 const dbCon = async ()=>{
-    mongoose.connect(urlDB)
+    mongoose.connect(urlDB,{ 
+        ssl: true 
+      })
 };
 module.exports = dbCon;
