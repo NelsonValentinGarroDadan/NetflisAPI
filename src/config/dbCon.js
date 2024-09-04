250836b6557477760dd22124a1c3698a635626c3
Velentin Garro <nelsonvgarrodadan@gmail.com>
2024-09-04 16:42:32 -0300

    Update dbCon.js

--- src/config/dbCon.js
+++ src/config/dbCon.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const urlDB = "mongodb+srv://nelsonvgarrodadan:zg5efej0vPIXA0xV@workplase.0pzcr.mongodb.net/movies"
+const urlDB = "mongodb+srv://nelsonvgarrodadan:zg5efej0vPIXA0xV@workplase.0pzcr.mongodb.net/?retryWrites=true&w=majority&appName=WorkPlase/Movies"
 const dbCon = async ()=>{
     mongoose.connect(urlDB)
 };
