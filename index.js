8d10006e0abc5a34cb5284cb6f271bdb345f3e13
Velentin Garro <nelsonvgarrodadan@gmail.com>
2024-09-04 16:59:48 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require('./src/server');
 const dbConfig = require('./src/config/dbCon')
-const port = 3000;
+const port = process.env.PORT || 3000;
 dbConfig()
     .then((res)=>{
         console.log("Se pudo conectar con exito la BD")
